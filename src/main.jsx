756bea653e91a01070272db626bd3a57c06bd1cf
kbfahim <kbfahim@konnect.bio>
2024-09-02 15:49:01 +0500

    df

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,17 @@ import { store } from './redux/store';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/React-Firebase-project/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    })
+    .catch((err) => {
+      console.log('Service Worker registration failed:', err);
+    });
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));  // Updated code
 
 root.render(
